34ac856a1e06eed9b2dcacb2c0e278d70c539264
minjie-shen <mjshen809@gmail.com>
2023-06-14 00:01:22 -0500

    [fix] Let gcloud successfully build on local

--- server/run.py
+++ server/run.py
@@ -3,7 +3,9 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+import os
 from api import app, db
+
 
 @app.shell_context_processor
 def make_shell_context():
@@ -11,5 +13,6 @@ def make_shell_context():
             "db": db
             }
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
