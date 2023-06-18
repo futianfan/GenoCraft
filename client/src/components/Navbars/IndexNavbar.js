b8c4ba41e42ea80b545deb1b16f8d1cc1f8c565d
minjie-shen <mjshen809@gmail.com>
2023-06-18 01:17:54 -0500

    [fix] Remove the sanity check timer

--- client/src/components/Navbars/IndexNavbar.js
+++ client/src/components/Navbars/IndexNavbar.js
@@ -25,7 +25,7 @@ export default function Navbar(props) {
               to="/"
               className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
             >
-              Homepage {currentTime}
+              Homepage
             </Link>
             <button
               className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
