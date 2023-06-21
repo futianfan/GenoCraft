657a94d1479d730da25e2992c3fbdd04d298116b
Tianfan Fu <tianfanfu@Tianfans-MacBook-Pro.local>
2023-06-21 16:36:16 -0400

    clean single-cell

--- single-cell/run_this.py
+++ single-cell/run_this.py
@@ -1,12 +1,13 @@
 from normalize import load_data, normalize_data
-from dimensionality_reduction import reduce_dimension
+from reduce_dimension import reduce_dimension
 from clustering import perform_clustering, plot_clusters
-from differential_analysis import differential_expression
+from differential_expression import differential_expression
 
 # Usage
-data = load_data('./rectum/read_count.tsv')
+data = load_data('./read_counts.csv')
 data_norm = normalize_data(data)
 tsne_result = reduce_dimension(data_norm)
 kmeans = perform_clustering(tsne_result)
 plot_clusters(tsne_result, kmeans, 'xxx.png')
 ttest_results = differential_expression(data, kmeans)
+print('success')
