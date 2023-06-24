be51fec85fc66ff425d23333477ac813b8cb7f55
Tianfan Fu <tianfanfu@Tianfans-MacBook-Pro.local>
2023-06-23 22:52:45 -0400

    reorganize

--- single-cell/visualization.py
+++ single-cell/visualization.py
@@ -1,9 +1,17 @@
+import io
 import matplotlib.pyplot as plt
-def plot_clusters(data, kmeans, filepath):
+
+def plot_clusters(data, kmeans):
+    fig, ax = plt.subplots()
+    stream = io.BytesIO()
     scatter = plt.scatter(data[:, 0], data[:, 1], c=kmeans.labels_, cmap='viridis')
     plt.title('t-SNE plot with KMeans clusters')
     handles, labels = scatter.legend_elements()
-    plt.savefig(filepath)
+    fig.savefig(stream, format='png')
+    stream.seek(0)
+    plt.close(fig)
+    return stream 
+    # plt.savefig(filepath)
 
 
 
