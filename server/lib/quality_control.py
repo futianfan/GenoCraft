c4e9f65a93c156c3f736de12c70a351842fe703d
Tianfan Fu <tianfanfu@Tianfans-MacBook-Pro.local>
2023-06-23 22:24:31 -0400

    quality control

--- server/lib/quality_control.py
+++ server/lib/quality_control.py
@@ -8,3 +8,12 @@ def filter_low_counts(df, min_counts=10, min_samples=3):
     min_samples (int): Minimum number of samples to keep a gene.
     """
     return df[(df >= min_counts).sum(axis=1) >= min_samples]
+
+
+if __name__ == "__main__":
+	import pandas as pd 
+	df = pd.read_csv('read_counts.csv', index_col=0)
+	df_filtered = filter_low_counts(df)
+	# Continue with normalization
+	# normalize_rnaseq_data(df_filtered, case_samples, control_samples)	
+
