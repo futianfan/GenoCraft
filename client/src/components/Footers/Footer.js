48cbf499d84390acdd9d601f31cddbb1abea971c
minjie-shen <mjshen809@gmail.com>
2023-06-24 17:22:39 -0500

    [feat] Fix the footer color inconsistency

--- client/src/components/Footers/Footer.js
+++ client/src/components/Footers/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <>
-            <footer className="relative bg-blueGray-200 pt-8 pb-6">
+            <footer className="relative pt-8 pb-6 bg-blueGray-200">
                 <div
                     className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
                     style={{transform: "translateZ(0)"}}
@@ -24,7 +24,7 @@ export default function Footer() {
                     </svg>
                 </div>
                 <div>
-                    <div className="flex flex-wrap items-center md:justify-between justify-center">
+                    <div className="flex flex-wrap items-center md:justify-between justify-center bg-blueGray-200">
                         <div className="w-full md:w-4/12 px-4 mx-auto text-center">
                             <div className="text-sm text-blueGray-500  py-1">
                                 Copyright © {new Date().getFullYear()} GenoCraft
