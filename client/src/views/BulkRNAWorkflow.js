e14e43bd036db11d49db9482b6f8061437c99b34
minjie-shen <mjshen809@gmail.com>
2023-06-24 21:59:28 -0500

    [fix] Fix Bulk UI color inconsistency

--- client/src/views/BulkRNAWorkflow.js
+++ client/src/views/BulkRNAWorkflow.js
@@ -395,7 +395,7 @@ export default function BulkRNAWorkflow() {
             <IndexNavbar fixed/>
             <section className="block relative z-1 bg-blueGray-100">
                 <div className="container mx-auto px-4 pb-32 pt-48 bg-blueGray-100">
-                    <div className="items-center flex flex-wrap">
+                    <div className="items-center flex flex-wrap bg-blueGray-100">
                         <div className="w-full md:w-5/12 ml-auto px-12 md:px-4">
                             <div className="md:pr-12">
                                 <div
