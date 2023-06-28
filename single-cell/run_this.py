4e94cf6be1fd3d1e76867381e83577cc495b7a9a
Tianfan Fu <tianfanfu@Tianfans-MacBook-Pro.local>
2023-06-27 21:01:22 -0400

    figure

--- single-cell/run_this.py
+++ single-cell/run_this.py
@@ -11,5 +11,19 @@ data_norm = load_data('GSE69405_PROCESSED_GENE_TPM_ALL_ready.txt')
 tsne_result = reduce_dimension(data_norm)
 kmeans = perform_clustering(tsne_result)
 stream = plot_clusters(tsne_result, kmeans)
+
+### stream to png
+file = open('GSE69405_clustering.png', 'wb')
+for chunk in stream:
+    file.write(chunk)
+file.close()
+### stream to png
+
+### DEG 
 ttest_results = differential_expression(data_norm, kmeans)
+
+
+### DDN 
+
+
 print('success')
