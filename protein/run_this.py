12763029824149b277c0b39a14c04d959b8f127d
Tianfan Fu <tianfanfu@Tianfans-MacBook-Pro.local>
2023-07-16 15:08:07 -0400

    bug in protein

---
+++ protein/run_this.py
@@ -0,0 +1,75 @@
+def save_stream_to_file2(stream, filepath):
+    with open(filepath, 'wb') as f:
+        f.write(stream.read())
+
+
+
+if __name__ == '__main__':
+    ### 1. quality control 
+    from GSEA import run_gsea_analysis, save_stream_to_file 
+
+    import pandas as pd 
+    import re 
+    from quality_control import filter_low_counts 
+    df = pd.read_csv('protein_expression.csv', sep = ',', dtype={0: str})
+    df_filtered = filter_low_counts(df)
+    
+    ### 1.5 randomly generating case_samples control_samples 
+    patient_names = df.columns.tolist()
+    print(patient_names)
+    case_samples = patient_names[:25]
+    control_samples = patient_names[25:]
+    print('case vs control:', case_samples, control_samples)
+    
+    ### 2.missing data imputation
+        # Filter out low counts
+    # Perform missing data imputation
+    df_imputed = impute_missing_data(df_filtered)
+
+    ### 3. normalize 
+    from Normalize import normalize_rnaseq_data 
+    # print(case_samples, control_samples)
+    df, case_df_cpm, control_df_cpm = normalize_rnaseq_data(df, case_samples, control_samples)
+    print('normalize', df.shape, case_df_cpm.shape, df, case_df_cpm)
+
+    ### 4. visualize 
+    case_df_cpm = case_df_cpm[:1000]
+    control_df_cpm = control_df_cpm[:1000]
+    print(df, case_df_cpm, control_df_cpm)
+    print(case_df_cpm.isna().sum())
+    print(control_df_cpm.isna().sum()) 
+    from Visualize import visualize  
+    stream = visualize(case_df_cpm, control_df_cpm) 
+    ### figure 
+    save_stream_to_file(stream, 'figure/clustering.png')
+
+
+    ### 5. differential analysis 
+    from differential_analysis import run_differential_analysis
+    # with open('read_counts.csv', 'r') as fin:
+    #     lines = fin.readlines()[1:]
+    #     gene_names = [line.split()[0] for line in lines] 
+    # genename_list = gene_names[:len(case_df_cpm)]
+    # print('deg', genename_list)
+    genename_list_short = gene_name_list[:1000]
+
+    significant_genes, significant_cases, significant_controls = \
+        run_differential_analysis(genename_list_short, case_df_cpm, control_df_cpm) 
+    ### figure 
+
+    column = significant_genes.columns.tolist()[0]
+    # print(column)
+    significant_genes = significant_genes[0].tolist() 
+    print('significant \n', significant_genes, len(significant_genes), type(significant_genes))
+    with open('significant_gene.txt', 'w') as fout:
+        for gene in significant_genes:
+            fout.write(gene + '\n') 
+
+    ### 5/6. GSEA 
+    from GSEA import run_gsea_analysis, save_stream_to_file 
+    gene_names_file = 'significant_gene.txt'
+    stream = run_gsea_analysis(gene_names_file, 'pathway_with_pvalues.csv')
+    save_stream_to_file(stream, 'figure/gsea.png')
+    ### figure 
+
+
