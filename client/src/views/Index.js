80b00604bbb3acbe1c0da6558e1586b56d0c5420
minjie-shen <mjshen809@gmail.com>
2023-10-24 22:24:46 -0500

    fix: fix mobile ui + remove unnecessary landing page

--- client/src/views/Index.js
+++ client/src/views/Index.js
@@ -15,7 +15,7 @@ export default function Index() {
             <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
                 <div className="container mx-auto items-center flex flex-wrap">
                     <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
-                        <div className="pt-32 sm:pt-0">
+                        <div className="pt-40 sm:pt-0">
                             <h2 className="font-semibold text-4xl text-blueGray-600">
                                 GenoCraft - A powerful, all-in-one solution for omics data processing
                             </h2>
