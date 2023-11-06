5ca43786ae264e8b723c857a93ead7c8cdb88884
minjie-shen <mjshen809@gmail.com>
2023-11-05 23:18:56 -0600

    feat: add two new authors

--- client/src/components/Footers/Footer.js
+++ client/src/components/Footers/Footer.js
@@ -4,25 +4,6 @@ export default function Footer() {
     return (
         <>
             <footer className="relative pt-8 pb-6 bg-blueGray-200">
-                <div
-                    className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
-                    style={{transform: "translateZ(0)"}}
-                >
-                    <svg
-                        className="absolute bottom-0 overflow-hidden"
-                        xmlns="http://www.w3.org/2000/svg"
-                        preserveAspectRatio="none"
-                        version="1.1"
-                        viewBox="0 0 2560 100"
-                        x="0"
-                        y="0"
-                    >
-                        <polygon
-                            className="text-blueGray-200 fill-current"
-                            points="2560 0 2560 100 0 100"
-                        ></polygon>
-                    </svg>
-                </div>
                 <div>
                     <div className="flex flex-wrap items-center md:justify-between justify-center bg-blueGray-200">
                         <div className="w-full md:w-4/12 px-4 mx-auto text-center">
