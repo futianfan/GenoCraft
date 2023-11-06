5cf817116e68b9eceff026756e906cc0c1874380
minjie-shen <mjshen809@gmail.com>
2023-11-06 00:29:58 -0600

    feat: update locust test file

--- server/locust/locustfile.py
+++ server/locust/locustfile.py
@@ -13,9 +13,36 @@ class GenoCraftUser(HttpUser):
             "number_of_files": 0,
             "quality_control": True,
             "imputation": True,
-            "normalization":True,
-            "visualization":True,
-            "differential_analysis":True,
-            "network_analysis":True,
+            "normalization": True,
+            "visualization": True,
+            "differential_analysis": True,
+            "network_analysis": False,
             "pathway_analysis": True
+        })
+
+    @task
+    def bulk_rna_workflow(self):
+        self.client.post("api/analyze/bulk", json={
+            "upload_own_file": False,
+            "quality_control": True,
+            "normalization": True,
+            "visualization_after_normalization": True,
+            "differential_analysis": True,
+            "network_analysis": False,
+            "gene_set_enrichment_analysis": True,
+            "visualization": True,
+            "number_of_files": 0
+        })
+
+    @task
+    def single_cell_workflow(self):
+        self.client.post("api/analyze/single-cell", json={
+            "upload_own_file": False,
+            "normalization": True,
+            "clustering": True,
+            "visualization": True,
+            "differential_analysis": True,
+            "network_analysis": False,
+            "pathway_analysis": True,
+            "number_of_files": 0,
         })
